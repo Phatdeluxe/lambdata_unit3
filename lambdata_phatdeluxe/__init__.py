bf24dca1b876eab22645e1cf2ca0acaf64047fe7
PhatDeluxe <ethan@skamarock.net>
2019-10-29 16:34:40 -0600

    created a class Data to be used with dataframes, moved all functions from init to df_utils, and updated version to 0.2.1

--- lambdata_phatdeluxe/__init__.py
+++ lambdata_phatdeluxe/__init__.py
@@ -4,33 +4,4 @@ Lambdata - a collection of DS helper functions
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-
-
-def split(data):
-    """
-    Makes a train, val, and test split from one dataframe
-    """
-    train, test = train_test_split(data, random_state=42, test_size=0.2)
-    train, val = train_test_split(train, random_state=42, test_size=0.2)
-    return train, val, test
-
-
-def check_null(data):
-    """
-    Prints the columns with and ammounts of null values
-    """
-    columns = data.columns
-    null_list = []
-    for column in columns:
-        if data[column].isnull().sum() > 0:
-            null_list.append({column: data[column].isnull().sum()})
-    for i in range(0, len(null_list)):
-        print(null_list[i], '\n')
-
-
-def add_to_df(to_series, name, df):
-    """
-    Takes a list and adds it to a dataframe as a new columns
-    """
-    new_col = pd.Series(to_series)
-    df[name] = new_col
+import lambdata_phatdeluxe
--- lambdata_phatdeluxe/df_utils.py
+++ lambdata_phatdeluxe/df_utils.py
@@ -1,5 +1,42 @@
 """
 utility functions for working with DataFrames
 """
-import pandas
-TEST_DF = pandas.DataFrame([1, 3, 4, 5, 6])
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+class Data:
+    """
+    For use with dataframes and the many things you want to do to them
+    """
+    def __init__(self, df):
+        self.df = df
+
+    def check_null(self):
+        """
+        Prints the columns with and ammounts of null values
+        """
+        columns = self.df.columns
+        null_list = []
+        for column in columns:
+            if self.df[column].isnull().sum() > 0:
+                null_list.append({column: self.df[column].isnull().sum()})
+        for i in range(0, len(null_list)):
+            print(null_list[i], '\n')
+
+    def split(self):
+        """
+        Makes a train, val, and test split from one dataframe
+        """
+        train, test = train_test_split(self.df, random_state=42, test_size=0.2)
+        train, val = train_test_split(train, random_state=42, test_size=0.2)
+        return train, val, test
+
+
+def add_to_df(to_series, name, df):
+    """
+    Takes a list and adds it to a dataframe as a new columns
+    """
+    new_col = pd.Series(to_series)
+    df[name] = new_col
